6fdcfc1ce40b76b00552bdff36e8e63c8b5c9c27
shamimsikder <shamimsikder138@gmail.com>
2023-05-03 04:11:04 +0600

    Load Chef Data

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
+const chef = require('./json/chef.json')
+
 app.get('/', (req, res) => {
 
     res.send("Chef Cuisine Explorer API is Running")
 
+})
+
+app.get('/chef', (req, res) => {
+    res.send(chef)
 })
 
 app.listen(port, () => {
