1188061ad17a044a0de22534f3bfbda566e5a665
shamimsikder <shamimsikder138@gmail.com>
2023-05-03 04:15:10 +0600

    Added CORS Middleware

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
+const cors = require('cors')
+app.use(cors())
+
 const chef = require('./json/chef.json')
 
 app.get('/', (req, res) => {
